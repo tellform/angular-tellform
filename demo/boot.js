2879a175e4cfc3e9955f5bd4eb510d603e0efe17
David Baldwynn <david.baldwin@alumni.ubc.ca>
2016-06-13 19:15:50 -0700

    refactored code

--- demo/boot.js
+++ demo/boot.js
@@ -22,53 +22,10 @@ angular.module('angular-tellform', [
     require('angular-bootstrap').name
 ]).constant('version', require('./package.json').version);
 
+//FIXME: App breaks when I remove this line and put modules in above statement
 angular.module('angular-tellform', ['ngResource', 'angular-tellform.templates']);
 
-angular.module('angular-tellform').filter('formValidity', require('../config/forms.client.config.js'));
-
-//Directives
-angular.module('angular-tellform').directive('angularTellform',
-    ['$rootScope', '$state', 'myForm', 'Auth',
-        require('../directives/render-form.client.directive.js')]);
-
-angular.module('angular-tellform').directive('fieldDirective',
-    ['$http', '$compile', '$rootScope', '$templateCache',
-        require('../directives/field.client.directive.js')]);
-
-angular.module('angular-tellform').directive('fieldIconDirective',
-    [require('../directives/field-icon.client.directive.js')]);
-
-angular.module('angular-tellform').directive('onEnterKey', ['$rootScope',
-    require('../directives/on-enter-key.client.directive.js')]);
-
-angular.module('angular-tellform').directive('onFinishRender',
-    require('../directives/on-finish-render.client.directive.js'));
-
-angular.module('angular-tellform').directive('submitFormDirective', ['$http', 'TimeCounter', '$filter', '$rootScope', 'Auth',
-    require('../directives/submit-form.client.directive.js')]);
-
-//Services
-angular.module('angular-tellform').factory('Auth', [ require('../services/auth.client.service.js') ]);
-
-angular.module('angular-tellform').factory('Current-Form', [ require('../services/current-form.client.service.js') ]);
-
-angular.module('angular-tellform').service('FormFields', [ require('../services/form-fields.client.service.js') ]);
-
-angular.module('angular-tellform').factory('Forms', ['$resource', 'FORM_URL', require('../services/forms.client.service.js') ]);
-
-angular.module('angular-tellform').factory('myForm', ['Forms', require('../services/myform.client.service.js')]);
-
-angular.module('angular-tellform').service('TimeCounter', [ require('../services/time-counter.client.service.js') ]);
-
-angular.module('angular-tellform').factory('$state', [function() {
-    return {
-        go: function() {}
-    };
-}]);
-
-//Constants
-angular.module('angular-tellform').constant('FORM_URL', '/form/:formId');
-
+require('./dist/form.js');
 
 angular.element(document).ready(function() {
 	//Then init the app
