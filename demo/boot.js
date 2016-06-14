836b7217e3eebddba3ee8a8390f1e2a917a91585
David Baldwynn <david.baldwin@alumni.ubc.ca>
2016-06-13 18:53:33 -0700

    changed module name to angular-tellform

--- demo/boot.js
+++ demo/boot.js
@@ -10,7 +10,7 @@ require('angular-strap');
 require('lodash');
 require('ng-file-upload');
 
-angular.module('TellForm', [
+angular.module('angular-tellform', [
     require('angular-ui-utils').name,
     require('angular-scroll').name,
     require('ui-select').name,
@@ -22,57 +22,57 @@ angular.module('TellForm', [
     require('angular-bootstrap').name
 ]).constant('version', require('./package.json').version);
 
-angular.module('TellForm', ['ngResource', 'TellForm.templates']);
+angular.module('angular-tellform', ['ngResource', 'angular-tellform.templates']);
 
-//angular.module('TellForm').requires.push('forms');
+//angular.module('angular-tellform').requires.push('forms');
 
-angular.module('TellForm').filter('formValidity', require('../config/forms.client.config.js'));
+angular.module('angular-tellform').filter('formValidity', require('../config/forms.client.config.js'));
 
 //Directives
-angular.module('TellForm').directive('fieldDirective',
+angular.module('angular-tellform').directive('fieldDirective',
     ['$http', '$compile', '$rootScope', '$templateCache',
         require('../directives/field.client.directive.js')]);
 
-angular.module('TellForm').directive('fieldIconDirective',
+angular.module('angular-tellform').directive('fieldIconDirective',
     [require('../directives/field-icon.client.directive.js')]);
 
-angular.module('TellForm').directive('onEnterKey', ['$rootScope',
+angular.module('angular-tellform').directive('onEnterKey', ['$rootScope',
     require('../directives/on-enter-key.client.directive.js')]);
 
-angular.module('TellForm').directive('onFinishRender',
+angular.module('angular-tellform').directive('onFinishRender',
     require('../directives/on-finish-render.client.directive.js'));
 
-angular.module('TellForm').directive('submitFormDirective', ['$http', 'TimeCounter', '$filter', '$rootScope', 'Auth',
+angular.module('angular-tellform').directive('submitFormDirective', ['$http', 'TimeCounter', '$filter', '$rootScope', 'Auth',
     require('../directives/submit-form.client.directive.js')]);
 
 //Services
-angular.module('TellForm').factory('Auth', [ require('../services/auth.client.service.js') ]);
+angular.module('angular-tellform').factory('Auth', [ require('../services/auth.client.service.js') ]);
 
-angular.module('TellForm').factory('Current-Form', [ require('../services/current-form.client.service.js') ]);
+angular.module('angular-tellform').factory('Current-Form', [ require('../services/current-form.client.service.js') ]);
 
-angular.module('TellForm').service('FormFields', [ require('../services/form-fields.client.service.js') ]);
+angular.module('angular-tellform').service('FormFields', [ require('../services/form-fields.client.service.js') ]);
 
-angular.module('TellForm').factory('Forms', ['$resource', 'FORM_URL', require('../services/forms.client.service.js') ]);
+angular.module('angular-tellform').factory('Forms', ['$resource', 'FORM_URL', require('../services/forms.client.service.js') ]);
 
-angular.module('TellForm').factory('myForm', ['Forms', require('../services/myform.client.service.js')]);
+angular.module('angular-tellform').factory('myForm', ['Forms', require('../services/myform.client.service.js')]);
 
-angular.module('TellForm').service('TimeCounter', [ require('../services/time-counter.client.service.js') ]);
+angular.module('angular-tellform').service('TimeCounter', [ require('../services/time-counter.client.service.js') ]);
 
-angular.module('TellForm').factory('$state', [function() {
+angular.module('angular-tellform').factory('$state', [function() {
     return {
         go: function() {}
     };
 }]);
 
 //Constants
-angular.module('TellForm').constant('FORM_URL', '/form/:formId');
+angular.module('angular-tellform').constant('FORM_URL', '/form/:formId');
 
 //Controllers
-angular.module('TellForm').controller('SubmitFormController',
+angular.module('angular-tellform').controller('SubmitFormController',
     ['$scope', '$rootScope', '$state', 'myForm', 'Auth',
         require('../controllers/submit-form.client.controller.js') ] );
 
 angular.element(document).ready(function() {
 	//Then init the app
-    angular.bootstrap(document, ['TellForm']);
+    angular.bootstrap(document, ['angular-tellform']);
 });
